aa4f201509c03edd6a79a6e6e6eb0a324b0e925a
hannemann <hannemann666@gmx.net>
2021-09-13 13:25:19 +0200

    feat: progress bar

--- resources/js/components/Progress/Current.js
+++ resources/js/components/Progress/Current.js
@@ -1,13 +1,37 @@
 import {PROGRESS_ITEM_CSS, ProgressItem} from './Items'
 
 
-class ProgressCurrent extends ProgressItem {}
+class ProgressCurrent extends ProgressItem {
+    get width() {
+        return `width: ${this.item.percentage}%;`
+    }
+}
 
 ProgressCurrent.template = /*html*/`
 ${PROGRESS_ITEM_CSS}
+<style>
+    .path {
+        position: relative;
+        height: 1rem;
+        width: 100%;
+    }
+    .path span {
+        position: absolute;
+        inset: 0;
+    }
+    .path span:last-of-type {
+        background: var(--clr-enlightened);
+        color: var(--clr-text-200-inverse);
+        overflow: hidden;
+        border-radius: .2rem;
+    }
+</style>
 <header>Current</header>
 <div>
-    <div class="path">{{ this.item.type.ucfirst() }}: {{ this.item.path }}</div>
+    <div class="path">
+        <span>{{ this.item.type.ucfirst() }}: {{ this.item.path }}</span>
+        <span style="{{ this.width }}">{{ this.item.type.ucfirst() }}: {{ this.item.path }}</span>
+    </div>
     <div>{{ this.item.percentage }}%</div>
 </div>
 `
