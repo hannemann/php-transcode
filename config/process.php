332b5360005e27e00c7dfb4677fa9664b0c234bd
hannemann <hannemann666@gmx.net>
2021-10-04 23:16:06 +0200

    fix: process config

--- config/process.php
+++ config/process.php
@@ -3,6 +3,6 @@
 return [
     'binaries' => [
         'ps' => env('BINARY_PS', '/usr/bin/ps'),
-        'kill' => env('BINARY_PS', '/usr/bin/kill'),
+        'kill' => env('BINARY_KILL', '/usr/bin/kill'),
     ]
 ];
