c43e5f268d2a07bfa9d2143018bdccb614c16746
hannemann <hannemann666@gmx.net>
2021-10-16 12:36:40 +0200

    feat: itroduce videoeditor base element

--- resources/js/components/Configurator/Dialogues/Clipper/index.js
+++ resources/js/components/Configurator/Dialogues/Clipper/index.js
@@ -1,22 +1,17 @@
-import { Slim, Utils, Iconify } from "@/components/lib";
+import { VideoEditor, EDITOR_TEMPLATE, EDITOR_CSS } from "../VideoEditor";
+import { Utils } from "@/components/lib";
 import { handleKey, rwd, ffwd } from "./mixins/handleKey";
 
 const THUMBNAIL_HEIGHT = 30;
-class Clipper extends Slim {
-    #aspect = "16:9";
-    #aspectDecimal = 16 / 9;
-    #height = 576;
-
+class Clipper extends VideoEditor {
     constructor() {
         super();
-        this.bindListeners();
-        this.current = 0;
         this.clips = [];
         this.raw = [];
-        this.height = 576;
     }
 
     bindListeners() {
+        super.bindListeners();
         this.rwd = rwd.bind(this);
         this.ffwd = ffwd.bind(this);
         this.handleKey = handleKey.bind(this);
@@ -25,19 +20,14 @@ class Clipper extends Slim {
         this.remove = this.remove.bind(this);
         this.timestamp = this.timestamp.bind(this);
         this.getClipPos = this.getClipPos.bind(this);
-        this.getIndicatorPos = this.getIndicatorPos.bind(this);
         this.activateClip = this.activateClip.bind(this);
-        this.handleIndicatorClick = this.handleIndicatorClick.bind(this);
     }
 
     onAdded() {
+        super.onAdded();
         this.current = parseInt(this.start * 1000, 10) ?? 0;
         document.addEventListener("keydown", this.handleKey);
-        requestAnimationFrame(() => {
-            Iconify.scan(this.shadowRoot);
-            this.addThumbnails();
-        });
-        this.calculateClips();
+        requestAnimationFrame(() => this.calculateClips());
     }
 
     onRemoved() {
@@ -70,42 +60,14 @@ class Clipper extends Slim {
         }
     }
 
-    addThumbnails() {
-        let i = 1;
-        const count = Math.floor(
-            this.indicator.offsetWidth / (THUMBNAIL_HEIGHT * (4 / 3))
-        );
-        const fr = (this.duration * 1000) / (count + 2);
-        do {
-            const img = document.createElement("img");
-            const timestamp = this.timestamp(fr * i);
-            img.src = `${this.baseUrl}${timestamp}&height=${THUMBNAIL_HEIGHT}`;
-            this.indicator.appendChild(img);
-        } while (i++ <= count);
-    }
-
-    handleIndicatorClick(e) {
-        if (!e.composedPath().find((p) => p.classList?.contains("clip"))) {
-            this.current =
-                (this.duration * 1000 * e.layerX) / this.indicator.offsetWidth;
-            Utils.forceUpdate(this);
-        }
-    }
-
-    activateClip(item) {
+    activateClip(e) {
+        const item = this.clips[e.target.dataset.index];
         if (this.current === item.raw.start && item.raw.end) {
             this.current = item.raw.end;
         } else {
             this.current = item.raw.start;
         }
         Utils.forceUpdate(this);
-    }
-
-    timestamp(current) {
-        return new Date(current ?? this.current)
-            .toISOString()
-            .replace(/^[0-9-]+T/, "")
-            .replace(/z$/i, "");
     }
 
     clipEndTimestamp(seconds) {
@@ -122,14 +84,19 @@ class Clipper extends Slim {
     }
 
     calculateClips() {
-        let clips = [];
+        this.clips.forEach((c) => {
+            c.node.removeEventListener("click", this.activateClip);
+            c.node.remove();
+        });
+        this.clips = [];
         for (let i = 0; i < this.raw.length; i += 2) {
             const start = this.raw[i] ? this.timestamp(this.raw[i]) : null;
             const end = this.raw[i + 1]
                 ? this.timestamp(this.raw[i + 1])
                 : null;
             const percentage = 100 / (this.duration * 1000);
-            clips.push({
+            const clip = {
+                index: this.clips.length,
                 timestamps: { start, end },
                 raw: {
                     start: start ? this.raw[i] : null,
@@ -141,14 +108,17 @@ class Clipper extends Slim {
                         ? percentage * (this.raw[i + 1] - this.raw[i])
                         : null,
                 },
-            });
+            };
+            clip.node = document.createElement("div");
+            clip.node.style.left = `${clip.percentage.start}%`;
+            clip.node.style.width = end ? `${clip.percentage.length}%` : "1px";
+            clip.node.dataset.index = clip.index;
+            clip.node.classList.add("clip");
+            clip.node.addEventListener("click", this.activateClip);
+            this.indicator.appendChild(clip.node);
+            this.clips.push(clip);
         }
-        this.clips = clips;
-    }
-
-    getIndicatorPos(item) {
-        const percentage = (100 / (this.duration * 1000)) * this.current;
-        return `left: min(${percentage}%, 100% - 1px`;
+        Utils.forceUpdate(this);
     }
 
     getClipPos(item) {
@@ -157,122 +127,23 @@ class Clipper extends Slim {
         }
         return `left: ${item.percentage.start}%;width:1px`;
     }
-
-    getFrameUrl() {
-        if (this.aspectDecimal) {
-            const width = this.height * this.aspectDecimal;
-            return `${this.baseUrl}${this.timestamp()}&width=${width}&height=${
-                this.height
-            }`;
-        }
-        return `${this.baseUrl}${this.timestamp()}`;
-    }
-
-    toggleAspect() {
-        switch (this.aspect) {
-            case "16:9":
-                this.aspect = "4:3";
-                this.aspectDecimal = 4 / 3;
-                break;
-            case "4:3":
-                this.aspect = "Native";
-                this.aspectDecimal = 0;
-                break;
-            default:
-                this.aspect = "16:9";
-                this.aspectDecimal = 16 / 9;
-        }
-        Utils.forceUpdate(this);
-    }
-
-    get aspectRatio() {
-        return this.aspect;
-    }
-
-    set aspectRatio(value) {
-        if (value.match(/([0-9]+):([0-9]+)/)) {
-            this.aspect = value;
-            this.aspectDecimal =
-                parseInt(RegExp.$1, 10) / parseInt(RegExp.$2, 10);
-            if (this.parentNode) {
-                Utils.forceUpdate(this);
-            }
-        }
-    }
-
-    get baseUrl() {
-        return `/image/${encodeURIComponent(this.path)}?timestamp=`;
-    }
 }
 
 Clipper.template = /*html*/ `
+${EDITOR_CSS}
 <style>
-    :host {
-        display: grid;
-        grid-template-columns: auto 1fr auto;
-        grid-template-rows: 1fr min-content max-content;
-        grid-template-areas:
-            "help frame timestamps"
-            "status status status"
-            "thumbnails thumbnails thumbnails";
-        grid-column-gap: .5rem;
-        height: 100%;
-    }
-
-    .frame {
-        grid-area: frame;
-        max-width: 100%;
-        max-height: 100%;
-        justify-self: center;
-    }
-    .status {
-        grid-area: status;
-        text-align: center;
-    }
-    .indicator {
-        grid-area: thumbnails;
-        height: ${THUMBNAIL_HEIGHT + 16}px;
-        position: relative;
-        --background: var(--clr-bg-200);
-        --size: 3px;
-        background-image:
-            linear-gradient(to right, var(--background) var(--size), transparent var(--size)),
-            linear-gradient(to bottom, var(--background) var(--size), transparent var(--size)),
-            linear-gradient(to right, var(--background) var(--size), transparent var(--size)),
-            linear-gradient(to bottom, var(--background) var(--size), transparent var(--size)),
-            linear-gradient(to bottom, transparent var(--size), var(--background) var(--size));
-        background-size: calc(var(--size) * 2) var(--size), calc(var(--size) * 2) var(--size), calc(var(--size) * 2) var(--size), calc(var(--size) * 2) var(--size), 100% calc(100% - var(--size) * 3);
-        background-repeat: repeat-x;
-        background-position: 0 var(--size), top left, 0 calc(100% - var(--size)), bottom left, 0 var(--size);
-        display: grid;
-        grid-auto-flow: column;
-        align-content: center;
-        justify-items: center;
-    }
-    .indicator img {
-        max-width: 100%;
-        max-height: 100%;
-    }
     .indicator .clip {
         position: absolute;
         inset-block: 0;
         background: hsla(var(--hue-success) var(--sat-alert) var(--lit-alert) / .5);
-    }
-    .indicator .current {
-        position: absolute;
-        inset-block: -3px;
-        background: red;
-        width: 1px;
+        z-index: 1;
     }
     .help, .clips {
         font-size: .75rem;
         white-space: nowrap;
     }
     .help {
-        grid-area: help;
-    }
-    .toggle-aspect::part(button) {
-        width: 100%;
+        grid-area: left;
     }
     .help dl {
         display: grid;
@@ -283,7 +154,7 @@ Clipper.template = /*html*/ `
         margin: 0;
     }
     .clips {
-        grid-area: timestamps;
+        grid-area: right;
         display: grid;
         grid-template-columns: 1fr;
         grid-auto-rows: min-content;
@@ -301,16 +172,8 @@ Clipper.template = /*html*/ `
         background: var(--clr-bg-200);
     }
 </style>
-<img class="frame" src="{{ this.getFrameUrl() }}" #ref="image">
-<div class="status">
-    {{ this.timestamp() }} / {{ this.timestamp(this.duration * 1000) }}
-</div>
-<div class="indicator" #ref="indicator" @click="{{ this.handleIndicatorClick }}">
-    <div class="clip" *foreach="{{ this.clips }}" style="{{ this.getClipPos(item) }}" @click="{{ this.activateClip(item) }}"></div>
-    <div class="current" #ref="indicatorCurrent" style="{{ this.getIndicatorPos(item) }}"></div>
-</div>
+${EDITOR_TEMPLATE}
 <div class="help">
-    <theme-button class="toggle-aspect" @click="{{ this.toggleAspect() }}">{{ this.aspect }}</theme-button>
     <dl>
         <dt><span class="iconify" data-icon="mdi-swap-horizontal-bold"></span></dt><dd>+/-1 Frame</dd>
         <dt><span class="iconify" data-icon="mdi-swap-horizontal-bold"></span> + Shift</dt><dd>+/-2 Seconds</dd>
@@ -339,12 +202,12 @@ Clipper.template = /*html*/ `
 </div>
 <div class="clips">
     <div class="clip" *foreach="{{ this.clips }}">
-        <div @click="{{ this.activateClip({raw:{start:item.raw.start}}) }}"
+        <div data-index="{{ item.index }}" @click="{{ this.activateClip }}"
             class="{{ item.raw.start === this.current ? 'timestamp active' : 'timestamp' }}"
         >
             {{ this.timestamp(item.raw.start) }}
         </div>
-        <div @click="{{ this.activateClip({raw:{start:item.raw.end}}) }}"
+        <div data-index="{{ item.index }}" @click="{{ this.activateClip }}"
             class="{{ item.raw.end === this.current ? 'timestamp active' : 'timestamp' }}"
         >
             {{ this.clipEndTimestamp(item.raw.end) }}
---
+++ resources/js/components/Configurator/Dialogues/VideoEditor/index.js
@@ -0,0 +1,186 @@
+import { Slim, Utils, Iconify } from "@/components/lib";
+
+const THUMBNAIL_HEIGHT = 30;
+
+class VideoEditor extends Slim {
+    constructor() {
+        super();
+        this.aspect = "16:9";
+        this.aspectDecimal = 16 / 9;
+        this.height = 576;
+        this.current = 0;
+        this.bindListeners();
+    }
+
+    bindListeners() {
+        this.handleIndicatorClick = this.handleIndicatorClick.bind(this);
+        this.getIndicatorPos = this.getIndicatorPos.bind(this);
+    }
+
+    onAdded() {
+        requestAnimationFrame(() => {
+            Iconify.scan(this.shadowRoot);
+            this.addThumbnails();
+        });
+    }
+
+    getIndicatorPos(item) {
+        const percentage = (100 / (this.duration * 1000)) * this.current;
+        return `left: min(${percentage}%, 100% - 1px`;
+    }
+
+    addThumbnails() {
+        let i = 1;
+        const count = Math.floor(
+            this.indicator.offsetWidth / (THUMBNAIL_HEIGHT * (4 / 3))
+        );
+        const fr = (this.duration * 1000) / (count + 2);
+        do {
+            const img = document.createElement("img");
+            const timestamp = this.timestamp(fr * i);
+            img.src = `${this.baseUrl}${timestamp}&height=${THUMBNAIL_HEIGHT}`;
+            this.indicator.appendChild(img);
+        } while (i++ <= count);
+    }
+
+    handleIndicatorClick(e) {
+        if (!e.composedPath().find((p) => p.classList?.contains("clip"))) {
+            this.current =
+                (this.duration * 1000 * e.layerX) / this.indicator.offsetWidth;
+            Utils.forceUpdate(this);
+        }
+    }
+
+    timestamp(current) {
+        return new Date(current ?? this.current)
+            .toISOString()
+            .replace(/^[0-9-]+T/, "")
+            .replace(/z$/i, "");
+    }
+
+    getFrameUrl() {
+        if (this.aspectDecimal) {
+            const width = this.height * this.aspectDecimal;
+            return `${this.baseUrl}${this.timestamp()}&width=${width}&height=${
+                this.height
+            }`;
+        }
+        return `${this.baseUrl}${this.timestamp()}`;
+    }
+
+    toggleAspect() {
+        switch (this.aspect) {
+            case "16:9":
+                this.aspect = "4:3";
+                this.aspectDecimal = 4 / 3;
+                break;
+            case "4:3":
+                this.aspect = "Native";
+                this.aspectDecimal = 0;
+                break;
+            default:
+                this.aspect = "16:9";
+                this.aspectDecimal = 16 / 9;
+        }
+        Utils.forceUpdate(this);
+    }
+
+    get baseUrl() {
+        return `/image/${encodeURIComponent(this.path)}?timestamp=`;
+    }
+
+    get aspectRatio() {
+        return this.aspect;
+    }
+
+    set aspectRatio(value) {
+        if (value.match(/([0-9]+):([0-9]+)/)) {
+            this.aspect = value;
+            this.aspectDecimal =
+                parseInt(RegExp.$1, 10) / parseInt(RegExp.$2, 10);
+            if (this.parentNode) {
+                Utils.forceUpdate(this);
+            }
+        }
+    }
+}
+
+export const EDITOR_CSS = /*html*/ `
+<style>
+:host {
+        display: grid;
+        grid-template-columns: auto 1fr auto;
+        grid-template-rows: 1fr min-content max-content;
+        grid-template-areas:
+            "left frame right"
+            "status status status"
+            "thumbnails thumbnails thumbnails";
+        grid-column-gap: .5rem;
+        height: 100%;
+    }
+
+    .frame {
+        grid-area: frame;
+        max-width: 100%;
+        max-height: 100%;
+        justify-self: center;
+    }
+    .status {
+        grid-area: status;
+        text-align: center;
+    }
+    .indicator {
+        grid-area: thumbnails;
+        height: ${THUMBNAIL_HEIGHT + 16}px;
+        position: relative;
+        --background: var(--clr-bg-200);
+        --size: 3px;
+        background-image:
+            linear-gradient(to right, var(--background) var(--size), transparent var(--size)),
+            linear-gradient(to bottom, var(--background) var(--size), transparent var(--size)),
+            linear-gradient(to right, var(--background) var(--size), transparent var(--size)),
+            linear-gradient(to bottom, var(--background) var(--size), transparent var(--size)),
+            linear-gradient(to bottom, transparent var(--size), var(--background) var(--size));
+        background-size: calc(var(--size) * 2) var(--size), calc(var(--size) * 2) var(--size), calc(var(--size) * 2) var(--size), calc(var(--size) * 2) var(--size), 100% calc(100% - var(--size) * 3);
+        background-repeat: repeat-x;
+        background-position: 0 var(--size), top left, 0 calc(100% - var(--size)), bottom left, 0 var(--size);
+        display: grid;
+        grid-auto-flow: column;
+        align-content: center;
+        justify-items: center;
+        z-index: 0;
+    }
+    .indicator img {
+        max-width: 100%;
+        max-height: 100%;
+        z-index: 0;
+    }
+    .indicator .current {
+        position: absolute;
+        inset-block: -3px;
+        background: red;
+        width: 1px;
+        z-index: 2;
+    }
+    .toggle-aspect {
+        grid-area: frame;
+        justify-self: start;
+        align-self: start;
+    }
+    .toggle-aspect::part(button) {
+        font-size: .75rem;
+        padding: .5rem;
+    }
+</style>`;
+
+export const EDITOR_TEMPLATE = /*html*/ `
+<img class="frame" src="{{ this.getFrameUrl() }}" #ref="image">
+<theme-button class="toggle-aspect" @click="{{ this.toggleAspect() }}">{{ this.aspect }}</theme-button>
+<div class="status">
+    {{ this.timestamp() }} / {{ this.timestamp(this.duration * 1000) }}
+</div>
+<div class="indicator" #ref="indicator" @click="{{ this.handleIndicatorClick }}">
+    <div class="current" #ref="indicatorCurrent" style="{{ this.getIndicatorPos(item) }}"></div>
+</div>`;
+
+export { VideoEditor };
