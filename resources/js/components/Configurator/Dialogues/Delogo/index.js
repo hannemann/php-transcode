8432ae1507a9630b1e721c6fcfcd63d35e1557dd
hannemann <hannemann666@gmx.net>
2021-10-29 07:57:10 +0200

    feat: add zoombox to delogo

--- resources/js/components/Configurator/Dialogues/Delogo/index.js
+++ resources/js/components/Configurator/Dialogues/Delogo/index.js
@@ -35,6 +35,20 @@ class DeLogo extends VideoEditor {
         console.info("Initialize Delogo");
         this.image.addEventListener("click", this.addDelogoBox);
         document.addEventListener("keydown", this.handleKey);
+        this.zoomImage = this.zoom.appendChild(document.createElement("img"));
+        this.zoomImage.src = this.image.src;
+    }
+
+    handleIndicatorClick(e) {
+        this.image.addEventListener(
+            "load",
+            () => {
+                console.log(this.image.src);
+                this.zoomImage.src = this.image.src;
+            },
+            { once: true }
+        );
+        super.handleIndicatorClick(e);
     }
 
     addDelogoBox(e) {
@@ -44,9 +58,28 @@ class DeLogo extends VideoEditor {
             this.delogoBox = document.createElement("div");
             this.shadowRoot.appendChild(this.delogoBox);
             this.delogoBox.classList.add("box");
+            this.zoomDelogoBox = this.zoom.appendChild(
+                this.delogoBox.cloneNode(true)
+            );
         }
         this.delogoBox.style.top = `${this.delogoOffsetTop}px`;
         this.delogoBox.style.left = `${this.delogoOffsetLeft}px`;
+        this.delogoBox.style.width = `20px`;
+        this.delogoBox.style.height = `20px`;
+        this.updateZoom();
+    }
+
+    updateZoom() {
+        const tr = `translate(
+            -${this.coords.x - this.zoom.offsetWidth / 2 + this.coords.w / 2}px,
+            -${this.coords.y - this.zoom.offsetHeight / 2 + this.coords.h / 2}px
+            )`;
+        this.zoomImage.style.transform = tr;
+        this.zoomDelogoBox.style.top = `${this.coords.y}px`;
+        this.zoomDelogoBox.style.left = `${this.coords.x}px`;
+        this.zoomDelogoBox.style.width = `${this.coords.w}px`;
+        this.zoomDelogoBox.style.height = `${this.coords.h}px`;
+        this.zoomDelogoBox.style.transform = tr;
     }
 
     handleKey(e) {
@@ -76,18 +109,27 @@ class DeLogo extends VideoEditor {
             } else {
                 const box = this.delogoBox.getBoundingClientRect();
                 if (e.key === "ArrowRight") {
-                    this.delogoBox.style.width = `${box.width + 1}px`;
+                    this.delogoBox.style.width = `${
+                        parseInt(this.delogoBox.style.width, 10) + 1
+                    }px`;
                 }
                 if (e.key === "ArrowLeft") {
-                    this.delogoBox.style.width = `${box.width - 1}px`;
+                    this.delogoBox.style.width = `${
+                        parseInt(this.delogoBox.style.width, 10) - 1
+                    }px`;
                 }
                 if (e.key === "ArrowDown") {
-                    this.delogoBox.style.height = `${box.height + 1}px`;
+                    this.delogoBox.style.height = `${
+                        parseInt(this.delogoBox.style.height, 10) + 1
+                    }px`;
                 }
                 if (e.key === "ArrowUp") {
-                    this.delogoBox.style.height = `${box.height - 1}px`;
+                    this.delogoBox.style.height = `${
+                        parseInt(this.delogoBox.style.height, 10) - 1
+                    }px`;
                 }
             }
+            this.updateZoom();
         }
     }
 
@@ -116,11 +158,26 @@ ${EDITOR_CSS}
     .box {
         position: absolute;
         background-color: hsla(0 100% 50% / .5);
-        width: 20px;
-        height: 20px;
+    }
+    .info {
+        grid-area: left;
+        display: grid;
+        grid-auto-rows: min-content;
+        gap: .5rem;
+        font-size: .75rem;
+        white-space: nowrap;
+    }
+    .zoom {
+        width: 250px;
+        aspect-ratio: 1;
+        overflow: hidden;
+        position: relative;
     }
 </style>
 ${EDITOR_TEMPLATE}
+<div class="info">
+    <div class="zoom" #ref="zoom"></div>
+</div>
 `;
 
 customElements.define("dialogue-delogo", DeLogo);
