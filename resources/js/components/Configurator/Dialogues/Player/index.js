192cf14f4ebb237bcabe9f5b98866d5095dd096a
Hannemann <h.kuenne@web.de>
2025-10-14 14:47:19 +0200

    video player max-width

--- resources/js/components/Configurator/Dialogues/Player/index.js
+++ resources/js/components/Configurator/Dialogues/Player/index.js
@@ -34,7 +34,7 @@ Player.template = /*html*/ `
     video {
         width: 100%;
         max-height: 99%;
-        max-width: 100%;
+        max-width: 1920px;
     }
 </style>
 <video #ref="video" controls></video>
