78e6d5803842a795010c6a07e0e32a34f8278365
Hannemann <h.kuenne@web.de>
2025-10-17 17:01:47 +0200

    remove babel and stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,32 +1,9 @@
 const mix = require('laravel-mix');
 const path = require('path')
 
-mix.babelConfig({
-    presets: [
-        "@babel/preset-env"
-    ],
-    plugins: [
-        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
-        ["@babel/plugin-proposal-class-properties", { loose: true }],
-        ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
-        ["@babel/plugin-proposal-private-methods", { "loose": true }]
-    ],
-});
-
 mix.alias({
     '@': path.join(__dirname, 'resources/js')
 });
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css')
