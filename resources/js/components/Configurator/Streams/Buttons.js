740c0572ca942169b3bb130e7a6667aea1d867f4
Hannemann <h.kuenne@web.de>
2026-01-18 11:04:40 +0100

    remove slim from stream buttons

--- resources/js/components/Configurator/Streams/Buttons.js
+++ resources/js/components/Configurator/Streams/Buttons.js
@@ -1,21 +1,34 @@
-import { Slim, Utils } from '@/components/lib';
+import { DomHelper } from '../../../Helper/Dom';
 import Iconify from '@iconify/iconify'
 import { ICON_STACK_CSS } from '@/components/Icons/Stack.css'
 
-class Buttons extends Slim {
+class Buttons extends HTMLElement {
 
-    onAdded() {
+    connectedCallback() {
+        DomHelper.initDom.call(this);
+        this.toggle = this.toggle.bind(this);
+        this.configure = this.configure.bind(this);
+        this.btnCheckbox = this.shadowRoot.querySelector('button[data-type="checkbox"]');
+        this.btnConfig = this.shadowRoot.querySelector('button[data-type="config"]');
+        this.btnCheckbox.addEventListener('click', this.toggle);
+        this.btnConfig.addEventListener('click', this.configure);
+        this.active = this.item.active;
         requestAnimationFrame(() => Iconify.scan(this.shadowRoot))
+    }
+
+    disconnectedCallback() {
+        this.btnCheckbox.removeEventListener('click', this.toggle);
+        this.btnConfig.removeEventListener('click', this.configure);
     }
 
     toggle() {
         this.item.active = !this.item.active
+        this.active = this.item.active;
         document.dispatchEvent(new CustomEvent('stream-toggle', {detail: {
             origin: this,
             item: this.item
         }}))
         requestAnimationFrame(() => {
-            Utils.forceUpdate(this, 'item')
             Iconify.scan(this.shadowRoot)
         })
     }
@@ -25,6 +38,11 @@ class Buttons extends Slim {
             origin: this,
             item: this.item
         }}))
+    }
+
+    set active(value) {
+        this.btnCheckbox.classList.toggle('active', !!value);
+        this.btnConfig.disabled = !value;
     }
 }
 
@@ -49,12 +67,12 @@ Buttons.template = /*html*/`
 </style>
 ${ICON_STACK_CSS}
 <main>
-    <button @click="{{ this.toggle() }}" class="{{ this.item.active ? 'icon-stack active' : 'icon-stack' }}">
+    <button data-type="checkbox" class="icon-stack">
         <span class="iconify active" data-icon="mdi-checkbox-marked-outline"></span>
         <span class="iconify inactive" data-icon="mdi-checkbox-blank-outline"></span>
         <span class="iconify hover" data-icon="mdi-checkbox-blank-outline"></span>
     </button>
-    <button @click="{{ this.configure() }}" class="icon-stack" .disabled="{{ !this.item.active }}">
+    <button data-type="config" class="icon-stack">
         <span class="iconify" data-icon="mdi-cog-outline"></span>
         <span class="iconify hover" data-icon="mdi-cog-outline"></span>
     </button>
