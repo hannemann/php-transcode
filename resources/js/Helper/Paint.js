a83508e97d7f1953aa1db9eadf5e1e4b3a366f66
Hannemann <h.kuenne@web.de>
2026-03-08 12:25:03 +0100

    add brush preset buttons

--- resources/js/Helper/Paint.js
+++ resources/js/Helper/Paint.js
@@ -15,6 +15,13 @@ const COLOR_SCHEME = {
     hoverControlContent: "var(--clr-text-100)",
 };
 
+const BRUSH_PRESETS = [
+    { name: "Small", size: 20, color: "#000000" },
+    { name: "Medium", size: 100, color: "#000000" },
+    { name: "Large", size: 500, color: "#000000" },
+    { name: "White", size: 150, color: "#FFFFFF" },
+];
+
 const ID = "painterro-paintarea";
 const paintArea = document.createElement("div");
 paintArea.id = ID;
@@ -63,6 +70,63 @@ export default class Paint {
                     iconUrl: Paint.filterLogoPixelsIcon,
                     callBack: () => {
                         Paint.openFilterLogoPixelsDialog();
+                        Paint.Painterro.closeActiveTool(
+                            Paint.Painterro.activeTool,
+                        );
+                    },
+                },
+                {
+                    name: "Small Brush White",
+                    iconUrl: Paint.filterLogoPixelsIcon,
+                    callBack: () => {
+                        const color = {
+                            palleteColor: "#FFFFFF",
+                            alpha: 1,
+                            alphaColor: "#FFFFFF",
+                        };
+                        Paint.Painterro.setLineWidth(20);
+                        Paint.Painterro.setColor.call(Paint.Painterro, [
+                            "bg",
+                            color,
+                        ]);
+                        Paint.Painterro.closeActiveTool(
+                            Paint.Painterro.activeTool,
+                        );
+                    },
+                },
+                {
+                    name: "Medium Brush Black",
+                    iconUrl: Paint.filterLogoPixelsIcon,
+                    callBack: () => {
+                        const color = {
+                            palleteColor: "#000000",
+                            alpha: 1,
+                            alphaColor: "#000000",
+                        };
+                        Paint.Painterro.setLineWidth(150);
+                        Paint.Painterro.setColor.call(Paint.Painterro, [
+                            "bg",
+                            color,
+                        ]);
+                        Paint.Painterro.closeActiveTool(
+                            Paint.Painterro.activeTool,
+                        );
+                    },
+                },
+                {
+                    name: "Big Brush Black",
+                    iconUrl: Paint.filterLogoPixelsIcon,
+                    callBack: () => {
+                        const color = {
+                            palleteColor: "#000000",
+                            alpha: 1,
+                            alphaColor: "#000000",
+                        };
+                        Paint.Painterro.setLineWidth(500);
+                        Paint.Painterro.setColor.call(Paint.Painterro, [
+                            "bg",
+                            color,
+                        ]);
                         Paint.Painterro.closeActiveTool(
                             Paint.Painterro.activeTool,
                         );
@@ -293,9 +357,19 @@ export default class Paint {
         return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
     }
 
+    /**
+     * Obtain logo filter icon with color from COLOR_SCHEME
+     */
     static get filterLogoPixelsIcon() {
-        const svg =
-            '<svg xmlns="http://www.w3.org/2000/svg" fill="${COLOR_SCHEME.controlContent}" viewBox="0 0 24 24"><path d="M15,3H12V6H15V3M19,3H16V6H19V3M15,7H12V10H15V7M19,7H16V10H19V7M11,3H8V6H11V3M7,3H4V6H7V3M11,7H8V10H11V7M7,7H4V10H7V7M21,11V21H3V11H21M19,19V13H5V19H19Z"/></svg>';
+        const svg = `<svg fill="${COLOR_SCHEME.controlContent}" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M487.976 0H24.028C2.71 0-8.047 25.866 7.058 40.971L192 225.941V432c0 7.831 3.821 15.17 10.237 19.662l80 55.98C298.02 518.69 320 507.493 320 487.98V225.941l184.947-184.97C520.021 25.896 509.338 0 487.976 0z"></path></g></svg>`;
+        return `data:image/svg+xml;base64,${btoa(svg)}`;
+    }
+
+    /**
+     * Obtain brush icon with color from COLOR_SCHEME
+     */
+    static get brushIcon() {
+        const svg = `<svg fill="${COLOR_SCHEME.controlContent}" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 444.892 444.892" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="XMLID_476_"> <path id="XMLID_503_" d="M440.498,173.103c5.858-5.857,5.858-15.355,0-21.213l-22.511-22.511c-5.091-5.091-13.084-5.846-19.038-1.8 l-47.332,32.17l31.975-47.652c3.993-5.951,3.219-13.897-1.85-18.964l-48.83-48.83c-4.508-4.508-11.372-5.675-17.114-2.908 l-8.443,4.065l4.043-8.97c2.563-5.685,1.341-12.361-3.068-16.771L293.002,4.393c-5.857-5.857-15.355-5.857-21.213,0 l-119.06,119.059l168.71,168.71L440.498,173.103z"></path> <path id="XMLID_1199_" d="M130.56,145.622l-34.466,34.466c-2.813,2.813-4.394,6.628-4.394,10.606s1.58,7.794,4.394,10.606 l32.694,32.694c6.299,6.299,9.354,14.992,8.382,23.849c-0.971,8.851-5.843,16.677-13.366,21.473 C27.736,340.554,18.781,349.51,15.839,352.453c-21.119,21.118-21.119,55.48,0,76.6c21.14,21.14,55.504,21.098,76.6,0 c2.944-2.943,11.902-11.902,73.136-107.965c4.784-7.505,12.607-12.366,21.462-13.339c8.883-0.969,17.575,2.071,23.859,8.354 l32.694,32.694c5.857,5.857,15.356,5.857,21.213,0l34.467-34.467L130.56,145.622z M70.05,404.825c-8.28,8.28-21.704,8.28-29.983,0 c-8.28-8.28-8.28-21.704,0-29.983c8.28-8.28,21.704-8.28,29.983,0C78.33,383.121,78.33,396.545,70.05,404.825z"></path> </g> </g></svg>`;
         return `data:image/svg+xml;base64,${btoa(svg)}`;
     }
 
@@ -323,15 +397,42 @@ const STATUS_CSS = css`
                 aspect-ratio: 1;
                 background-color: var(--clr-text-0);
 
-                -webkit-mask-image: url(${Paint.filterLogoPixelsIcon});
-                mask-image: url(${Paint.filterLogoPixelsIcon});
-
                 -webkit-mask-repeat: no-repeat;
                 mask-repeat: no-repeat;
                 -webkit-mask-position: center;
                 mask-position: center;
                 -webkit-mask-size: contain;
                 mask-size: contain;
+            }
+
+            &[title="Filter Logo Pixels"]::after {
+                -webkit-mask-image: url(${Paint.filterLogoPixelsIcon});
+                mask-image: url(${Paint.filterLogoPixelsIcon});
+            }
+
+            &[title*=" Brush "]::after {
+                -webkit-mask-image: url(${Paint.brushIcon});
+                mask-image: url(${Paint.brushIcon});
+            }
+
+            &[title*=" Brush White"]::after {
+                background-color: white;
+            }
+
+            &[title*=" Brush Black"]::after {
+                background-color: black;
+            }
+
+            &[title*="Small Brush"]::after {
+                width: 0.6rem;
+            }
+
+            &[title*="Medium Brush"]::after {
+                width: 0.9rem;
+            }
+
+            &[title*="Big Brush"]::after {
+                width: 1.2rem;
             }
         }
 
