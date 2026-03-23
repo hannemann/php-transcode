490f9d36f1c6b4e3329c8d93f55fc81a4f89e652
Hannemann <h.kuenne@web.de>
2026-03-23 08:08:47 +0100

    attempt to fix kill ffmpeg with custom filename

--- app/Models/FFMpeg/Actions/KillFFMpeg.php
+++ app/Models/FFMpeg/Actions/KillFFMpeg.php
@@ -5,6 +5,7 @@ namespace App\Models\FFMpeg\Actions;
 use App\Models\CurrentQueue;
 use App\Models\Drivers\PsDriver;
 use Illuminate\Support\Str;
+use App\Helper\Settings;
 
 class KillFFMpeg
 {
@@ -19,12 +20,16 @@ class KillFFMpeg
         });
     }
 
-    private static function getProcess(string $path):? string
+    private static function getProcess(string $path): ?string
     {
+        $filename = Settings::getSettings($path)['outFile'];
+        if (!$filename) {
+            $filename = sha1($path);
+        }
         $driver = PsDriver::load(config('process.binaries.ps'));
         $processList = collect(explode("\n", $driver->command(['-axo', 'pid,command'])));
-        $idx = $processList->search(function ($item) use ($path) {
-            return Str::containsAll($item, ['ffmpeg', sha1($path)]);
+        $idx = $processList->search(function ($item) use ($filename) {
+            return Str::containsAll($item, ['ffmpeg', $filename]);
         });
         return $idx ? explode(' ', trim($processList[$idx]))[0] : null;
     }
