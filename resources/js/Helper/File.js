1e2547f8502d82127d0a8583fe4132bb3a72e555
Hannemann <h.kuenne@web.de>
2026-07-05 09:49:19 +0200

    remove numbering from end of filename

--- resources/js/Helper/File.js
+++ resources/js/Helper/File.js
@@ -14,9 +14,15 @@ class FileHelper {
         const hasEpisode = FileHelper.#hasEpisode(fileName);
         if (hasEpisode) {
             const { episode, title, ext } = hasEpisode.groups;
-            fileName = `${episode}.${title}.${ext}`;
+            const cleanTitle = title
+                .replace(/-[0-9]+$/, "")
+                .replace(/---/g, " - ");
+            fileName = `${episode}.${cleanTitle}.${ext}`;
         } else {
+            const numberPattern = `-[0-9]+\\.(${VIDEO_EXTENSIONS.join("|")})$`;
+            const numberReg = new RegExp(numberPattern);
             fileName = fileName
+                .replace(numberReg, ".$1")
                 .replace(/---/g, " - ")
                 .replace(/([^\s])-([^\s])/g, "$1 $2");
         }
